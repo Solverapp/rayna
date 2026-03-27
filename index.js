20237b5aa01fcdcb3017cb4d073fc9d302ee77fc
Solverapp <hovo1601@gmail.com>
2026-03-27 18:22:41 +0400

    Update index.js3

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(express.json());
 
 // Rayna API base
-const BASE_URL = "https://activities.raynatours.com/api/Tour";
+const BASE_URL = "https://activities.raynatours.com/api";
 
 // Keep-Alive агент для HTTPS
 const agent = new https.Agent({ keepAlive: true, rejectUnauthorized: false });
